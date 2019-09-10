c8e6080c74519f8100aab76c5d5c2bed9f5675d8
brandiw <brandimichelle.0911@gmail.com>
2019-09-10 11:46:19 -0700

    About to deploy partial app

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -14,4 +14,36 @@ router.get('/', (req, res) => {
   })
 })
 
+// GET /v1/bounties/:id
+router.get('/:id', (req, res) => {
+  res.send('STUB for GET /bounties/:id')
+})
+
+// POST /v1/bounties
+router.post('/', (req, res) => {
+  db.Bounty.create(req.body)
+  .then(bounty => {
+    res.status(201).send(bounty)
+  })
+  .catch(err => {
+    console.log('Error while creating', err)
+    res.status(500).send({ message: 'Server Error' })
+  })
+})
+
+// PUT /v1/bounties/:id
+router.put('/:id', (req, res) => {
+  res.send('STUB for PUT /bounties/:id')
+})
+
+// DELETE /v1/bounties
+router.delete('/', (req, res) => {
+  res.send('STUB for DELETE /bounties')
+})
+
+// DELETE /v1/bounties
+router.delete('/:id', (req, res) => {
+  res.send('STUB for DELETE /bounties/:id')
+})
+
 module.exports = router
